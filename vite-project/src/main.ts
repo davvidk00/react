f14f7bf1e08c7368743dc985d20483450bce98a8
DawidC <davvidcapar@gmail.com>
2023-05-12 14:17:34 +0200

    a

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { setupCounter } from '../../dist/counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
